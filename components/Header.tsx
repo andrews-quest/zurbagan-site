d030a2eb21d89cec0376888c3fbf002b523c32d7
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-05-28 21:58:13 +0200

    Banner is wider, higher and contains menu buttons.

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,13 +16,13 @@ export const Header: React.FC<{
     return (
         <header>
             <div id="banner">
-               <h1>{textHeader}</h1>
-            </div>
-            <div id="buttons">
-                <a><div id="button" ><p>ПРО НАC</p></div></a>
-                <a><div id="button"><p>АБОНЕНТАМ</p></div></a>
-                <a><div id="button"><p>ПІДКЛЮЧИТИСЯ</p></div></a>
-                <a><div id="button"><p>НОВИНИ</p></div></a>
+                <h1>{textHeader}</h1>
+                <div id="buttons">
+                    <a><div id="button" ><p>ПРО НАC</p></div></a>
+                    <a><div id="button"><p>АБОНЕНТАМ</p></div></a>
+                    <a><div id="button"><p>ПІДКЛЮЧИТИСЯ</p></div></a>
+                    <a><div id="button"><p>НОВИНИ</p></div></a>
+                </div>
             </div>
         </header>
     );
