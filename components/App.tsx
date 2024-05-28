5a73fcbbee9f8d4330e8e42eb0df071ed3d50696
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-05-28 19:49:27 +0200

    Added Footer.

--- components/App.tsx
+++ components/App.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import  ReactDOM from "react-dom/client"
 import {Header} from "./Header"
 import {PostsDisplay} from "./PostsDisplay"
+import {Footer} from "./Footer"
 import "./App.css"
 
 export default function App() {
     return <>
     <Header textHeader={"ЗУРБАГАН"} />
     <PostsDisplay />
+    <Footer />
     </>
 }
 
---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css"
+
+export const Footer: React.FC <{
+
+}> = ({
+
+}) => {
+    return (
+        <footer>
+            <a>
+            TOV Zurbagan
+            Tel. +3800000000 
+            </a>
+        </footer>
+    )
+}
