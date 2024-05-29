9fa4d37c50bfb611ffb3038a41c034d6f82a7229
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-05-29 20:28:56 +0200

    Added heading and hover to offers tab.

--- components/OffersDisplay.tsx
+++ components/OffersDisplay.tsx
@@ -9,15 +9,18 @@ export const OffersDisplay: React.FC <{
 }) => {
     return (
         <div id="offers_display">
-            <Offer 
-                offer_name={"FiberNET - 50"}
-                offer_info={["Швидкість до 50Мбіт/с", "Безкоштовне підключення", "150 грн./міс."]}/>
-            <Offer
-                offer_name={"FiberNET - 100"}
-                offer_info={["Швидкість до 100Мбіт/с", "Безкоштовне підключення", "200 грн./міс."]}/>
-            <Offer 
-                offer_name={"ТБ - Базовий"}
-                offer_info={["90 каналів у пакеті", "Перше підключення 30 грн.", "90 грн./міс."]}/>
+            <h2>Рекомендовані Тарифи</h2>
+            <div id="offers_div">
+                <Offer 
+                    offer_name={"FiberNET - 50"}
+                    offer_info={["Швидкість до 50Мбіт/с", "Безкоштовне підключення", "150 грн./міс."]}/>
+                <Offer
+                    offer_name={"FiberNET - 100"}
+                    offer_info={["Швидкість до 100Мбіт/с", "Безкоштовне підключення", "200 грн./міс."]}/>
+                <Offer 
+                    offer_name={"ТБ - Базовий"}
+                    offer_info={["90 каналів у пакеті", "Перше підключення 30 грн.", "90 грн./міс."]}/>
+            </div>
         </div>
     )
 }
