cd37374d1ced0f0c076c53726a6e0e06e819321c
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-05-29 21:54:19 +0200

    Added first icon to big menu.

--- components/BigMenu.tsx
+++ components/BigMenu.tsx
@@ -1,6 +1,9 @@
 import React from "react"
 import "./BigMenu.css"
 
+import payIconGreen from "../resources/images/payment_green.png"
+import payIconWhite from "../resources/images/payment_white.png"
+
 export const BigMenu : React.FC <{
 
 }> = ({
@@ -8,10 +11,18 @@ export const BigMenu : React.FC <{
 }) => {
     return (
             <div id="buttons">
+                    <a><div id="button">
+                        <img></img>
+                        <h2>ПІДКЛЮЧИТИСЯ</h2></div></a>
+                    <a><div id="button">
+                            <div>
+                                <img src={payIconWhite} id="white"></img>
+                                <img src={payIconGreen} id="green"></img>
+                            </div>
+                            <h2>СПЛАТИТИ</h2>
+                        </div></a>
+                    <a><div id="button"><h2>CЛУЖБА ПІДТРИМКИ</h2></div></a>
                     <a><div id="button" ><h2>ПРО НАC</h2></div></a>
-                    <a><div id="button"><h2>АБОНЕНТАМ</h2></div></a>
-                    <a><div id="button"><h2>ПІДКЛЮЧИТИСЯ</h2></div></a>
-                    <a><div id="button"><h2>CЛУЖБА<br/>ПІДТРИМКИ</h2></div></a>
                 </div>
     )
 }
