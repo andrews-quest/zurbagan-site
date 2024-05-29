04b76973c9b28249bf4fbe9b43748b701da7ad9f
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-05-29 18:13:18 +0200

    Formatted navbar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,15 +9,17 @@ export const Navbar: React.FC <{
 }) => {
     return (
         <div id="navbar">
-            <div>
+            <div id="navbar_logo">
                 <img src={logo}></img>
             </div>
             <div id="navbar_menu">
                 <div><p>Aбонентам</p></div>
+                <div><p>Бізнесу</p></div>
                 <div><p>Інтернет</p></div>
                 <div><p>Телебачення</p></div>
+                <div><p>Оплата</p></div>
             </div>
-            <div>
+            <div id="navbar_button_container">
                 <div id="account_button">Мій Кабінет</div>
             </div>
         </div>
